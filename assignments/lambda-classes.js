7dd0cb7634ee43040c7180f53fd0ba8c90cb0146
Your Name <aujones95@gmail.com>
2019-04-11 16:04:14 -0400

    almost done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,41 @@
 // CODE here for your Lambda Classes
+
+const Person = new Instructor ({
+    name: 'Abby',
+    age: 26,
+    location:'New York',
+    gender:'female',
+    
+)}
+
+class Instructor extends Person{
+    constructor(props) {
+        super(props);
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.loctaion;
+        this.gender = props.gender;
+        specialty: 'Front-end';
+        favLanguage:'JavaScript';
+        cathPhrase: 'lets do this';
+    }
+}
+
+class Student extends Person {
+    constructor(props){
+        super(props);
+        previousBackground:'computerSciences';
+        className:'Cs243';
+        favSubject:'CSS,HTML';
+        }
+    
+}
+
+class ProjectManager extends instructor{
+    constructor(props) {
+        super(props);
+        gradClassName:cs113;
+        favInstructor:'John';
+        
+    }
+}
